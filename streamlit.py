ba29051b5d368ebe1d9241a62a65ca7b922de464
Tanya Nantsa <77226151+tanya-nantsa@users.noreply.github.com>
2024-08-26 19:24:25 -0400

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -26,13 +26,12 @@ ingredients_list = st.multiselect(
 )
 
 if ingredients_list:
-
     ingredients_string = ''
     
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
-
-    # st.write(ingredients_list)
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+        fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
             values ('""" + ingredients_string + """','""" + name_on_order+ """')"""
@@ -46,7 +45,4 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered!', icon="✅")
 
-# Section to display fruityvice nutrition information
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-# st.text(fruityvice_response.json())
-fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
+
