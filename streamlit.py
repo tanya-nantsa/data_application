2ba5ee9f8ddd6098e6f82f22e9b39a5bdf92f7c7
Tanya Nantsa <77226151+tanya-nantsa@users.noreply.github.com>
2024-08-26 19:21:37 -0400

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -48,5 +48,5 @@ if ingredients_list:
 
 # Section to display fruityvice nutrition information
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response.json())
-
+# st.text(fruityvice_response.json())
+fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
